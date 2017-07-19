50d8eb510aeea323e87f6fb01ea66ab8990ee62d
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-18 21:47:33 -0600

    Add parallax

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,67 @@
 <template>
   <section class="container">
-    <div>
-      <img class="title" src="/vandyhacks-text.svg" />
-      <h2 class="subtitle">
-        October 20-22 at Vanderbilt University
-      </h2>
-      <img class="batman-building" src="/batman.svg" />
+    <div class="top-viewport">
+      <div class="title-text parallax" data-rellax-speed="-10">
+        <img class="title" src="/vandyhacks-text.svg" />
+        <h2 class="subtitle">
+          October 20-22 at Vanderbilt University
+        </h2>
+      </div>
+      <img class="batman-building parallax" src="/batman.svg" data-rellax-speed="-5"/>
+    </div>
+    <div class="page-content">
+      aaa
     </div>
   </section>
 </template>
 
 <script>
+import Rellax from 'rellax'
 
 export default {
+  mounted() {
+    this.rellax = new Rellax('.parallax')
+  },
+  destroyed() {
+    this.rellax.destroy()
+  }
 }
 </script>
 
 <style>
 .container {
   min-height: 100vh;
+  flex-flow: column;
+}
+
+.top-viewport {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  height: 100vh;
+}
+
+.title-text {
+  margin-top: 20vh;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: -2;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  font-size: 36px;
+  color: #374c5f;
 }
 
-.links {
-  padding-top: 15px;
+.batman-building {
+  position: absolute;
+  max-width: 600px;
+  left: 0;
+  right: 0;
+  margin: 33vh auto 0;
+  z-index: -1;
+}
+
+.page-content {
+  flex: 2;
+  background: red;
 }
 </style>
