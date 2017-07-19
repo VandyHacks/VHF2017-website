a3801514dc608e51320f53531fe0053fdca5d434
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-18 19:32:16 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="container">
+    <div>
+      <img class="title" src="/vandyhacks-text.svg" />
+      <h2 class="subtitle">
+        October 20-22 at Vanderbilt University
+      </h2>
+      <img class="batman-building" src="/batman.svg" />
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
