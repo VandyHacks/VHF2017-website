3e3b536012360c8ceb8a2ea694a540c2a810c28d
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-18 19:41:56 -0600

    add fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,15 @@
 </template>
 
 <style>
-html
-{
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+@font-face {
+  font-family: 'Sofia Pro';
+  src: url("~assets/fonts/SofiaProRegular.woff2") format("woff2"), url("~assets/fonts/SofiaProRegular.woff") format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
+html {
+  font-family: 'Sofia Pro', sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,8 +22,10 @@ html
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-*, *:before, *:after
-{
+
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
