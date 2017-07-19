dbc1ae6303415ebadf1a568f0f05a831721c9b3e
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-19 09:30:45 -0600

    Add noopener

--- components/MlhBadge.vue
+++ components/MlhBadge.vue
@@ -1,5 +1,5 @@
 <template>
-    <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray" target="_blank">
+    <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray" rel="noopener" target="_blank">
         <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg" alt="Major League Hacking 2017 Hackathon Season" style="width:100%">
     </a>
 </template>
