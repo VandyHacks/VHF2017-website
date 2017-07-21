e4199be9570045b62028c63f4bdb2ad1054745d6
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-20 18:49:46 -0600

    Try white badge

--- components/MlhBadge.vue
+++ components/MlhBadge.vue
@@ -1,6 +1,6 @@
 <template>
-    <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray" rel="noopener" target="_blank">
-        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg" alt="Major League Hacking 2017 Hackathon Season" style="width:100%">
+    <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=white" rel="noopener" target="_blank">
+        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/white.svg" alt="Major League Hacking 2017 Hackathon Season" style="width:100%">
     </a>
 </template>
 
