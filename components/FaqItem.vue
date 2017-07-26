d91f20da287dd1e0eebe4a1fb2573dfe77837c24
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-26 15:09:25 -0400

    Add transition to faq item

--- components/FaqItem.vue
+++ components/FaqItem.vue
@@ -6,14 +6,38 @@
 .question {
   cursor: pointer
 }
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transform-origin: top;
+}
+
+.slide-fade-enter-active {
+  transition: transform .25s;
+}
+
+.slide-fade-leave-active {
+  transition: transform .1s;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: scaleY(0);
+}
+
+.slide-fade-enter-to {
+  transform: scaleY(1);
+}
 </style>
 
 <template>
   <div class="faq-item">
     <h4 class="question" @click="open = !open">{{ title }}</h4>
-    <div v-if="open">
-      <slot/>
-    </div>
+    <transition name="slide-fade">
+      <div v-if="open">
+        <slot/>
+      </div>
+    </transition>
   </div>
 </template>
 
