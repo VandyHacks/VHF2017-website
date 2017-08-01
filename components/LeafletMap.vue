b0bd6a451ac4ebda34d55b7836eadd872fe3f91b
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-01 09:10:20 -0400

    Attribution

--- components/LeafletMap.vue
+++ components/LeafletMap.vue
@@ -20,9 +20,11 @@ export default {
       const L = require('leaflet')
       this.map = L.map(this.$el, {
         center: this.center,
-        zoom: this.zoom
+        zoom: this.zoom,
+        attributionControl: false
       })
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiYmVuY29vcGVyMjIyIiwiYSI6ImNpeGlxY2dxMzAwMTYzMnBqdnM5ZzE0ZDgifQ.ny6yQcI2QEHve1aMhs-0Rw'
