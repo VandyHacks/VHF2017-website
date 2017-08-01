67c24093b4692e23772194d66c4f33d182f9b2b6
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-01 09:37:24 -0400

    Switch to recommended mapbox attribution

--- components/LeafletMap.vue
+++ components/LeafletMap.vue
@@ -42,7 +42,7 @@ export default {
       })
       this.map.attributionControl.setPrefix('')
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        attribution: '&copy; <a href="http://mapbox.com">Mapbox</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> <a href="https://www.mapbox.com/feedback/"><b>Improve this map</b></a>',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: this.accessToken
