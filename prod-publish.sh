9ba9adf96b7d1bf69f513c78f6e02d6a21939eb3
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-08-05 17:24:41 -0400

    Add prod-publish.sh

---
+++ prod-publish.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIST_DIR=$PWD/dist
+GH_PAGE_DIR=$PWD/../vandyhacks-site/
+
+npm run generate
+cd $GH_PAGE_DIR
+rm -rf *
+cp -r $DIST_DIR/* .
+git add -A
+git commit -m "Update site"
+git push
