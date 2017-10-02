7bf5dad888cac9abddb9f1736a24660673b27e53
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-02 12:12:17 -0500

    Apply -> Login

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
         <div class="logo">
           <img alt="VandyHacks IV" src="~assets/img/vandyhacks-text.svg" />
           <div class="actions">
-            <a ontouchstart="" class="apply-btn" href="https://apply.vandyhacks.org/login">Apply</a>
+            <a ontouchstart="" class="apply-btn" href="https://apply.vandyhacks.org/login">Login</a>
           </div>
         </div>
       </div>
