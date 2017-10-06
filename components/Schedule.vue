332692aeffe69b6934a24ad8a4a96d308c581576
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-05 20:48:27 -0500

    Base layout

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,7 +1,15 @@
 <style scoped>
 .sched-col {
     flex: 1;
-    margin: 20px 1.5em 0;
+    margin: 20px 2.5em 0;
+}
+
+.sched-col:first-child {
+    margin-left: 0;
+}
+
+.sched-col:last-child {
+    margin-right: 0;
 }
 
 .date {
@@ -11,10 +19,11 @@
 }
 
 .event {
-    margin-top: .5em;
+    margin-top: .75em;
 }
 
-.event-name, .event-time {
+.event-name,
+.event-time {
     display: inline-block;
 }
 
@@ -60,10 +69,73 @@
                     <div class="event-name">Beginner Tech Talks</div>
                     <div class="event-time">11:00p</div>
                 </div>
-
             </div>
         </div>
-        <div class="sched-col">Ayy lmao</div>
-        <div class="sched-col">Ayy lmao </div>
+        <div class="sched-col">
+            <h3 class="date">Saturday, Oct 21st</h3>
+            <div class="events">
+                <div class="event">
+                    <div class="event-name">Midnight Snack</div>
+                    <div class="event-time">12:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Intermediate Tech Talks</div>
+                    <div class="event-time">12:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Breakfast</div>
+                    <div class="event-time">8:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Advanced Tech Talks</div>
+                    <div class="event-time">9:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Lunch</div>
+                    <div class="event-time">12:00p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Dinner </div>
+                    <div class="event-time">5:30p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Werewolf, hosted by MLH</div>
+                    <div class="event-time">9:00p</div>
+                </div>
+            </div>
+        </div>
+        <div class="sched-col">
+            <h3 class="date">Sunday, Oct 22nd</h3>
+            <div class="events">
+                <div class="event">
+                    <div class="event-name">Midnight Snack</div>
+                    <div class="event-time">12:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Breakfast</div>
+                    <div class="event-time">8:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Hacking Ends</div>
+                    <div class="event-time">10:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">How to Demo Talk</div>
+                    <div class="event-time">10:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Lunch</div>
+                    <div class="event-time">11:00a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Project Expo</div>
+                    <div class="event-time">11:30a</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Closing Ceremony</div>
+                    <div class="event-time">1:30p</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
