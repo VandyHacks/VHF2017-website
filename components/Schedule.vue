e871928f56f8550d17c7e6094eb3f2724f3c2095
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-05 20:34:11 -0500

    Add prototype 1 col

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,3 +1,69 @@
+<style scoped>
+.sched-col {
+    flex: 1;
+    margin: 20px 1.5em 0;
+}
+
+.date {
+    text-decoration: underline;
+    font-weight: 400;
+    margin-bottom: 1em;
+}
+
+.event {
+    margin-top: .5em;
+}
+
+.event-name, .event-time {
+    display: inline-block;
+}
+
+.event-name {
+    font-weight: bold;
+}
+
+.event-time {
+    float: right;
+}
+</style>
+
 <template>
-    <div>ayy lmao</div>
+    <div class="row">
+        <div class="sched-col">
+            <h3 class="date">Friday, Oct 20th</h3>
+            <div class="events">
+                <div class="event">
+                    <div class="event-name">Check-in</div>
+                    <div class="event-time">5:30p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Dinner</div>
+                    <div class="event-time">6:00p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Opening Ceremony</div>
+                    <div class="event-time">8:00p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Hacking Begins</div>
+                    <div class="event-time">10:00p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Git Tech Talk</div>
+                    <div class="event-time">10:00p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Beginner Switch Session</div>
+                    <div class="event-time">10:30p</div>
+                </div>
+                <div class="event">
+                    <div class="event-name">Beginner Tech Talks</div>
+                    <div class="event-time">11:00p</div>
+                </div>
+
+            </div>
+        </div>
+        <div class="sched-col">Ayy lmao</div>
+        <div class="sched-col">Ayy lmao </div>
+    </div>
 </template>
